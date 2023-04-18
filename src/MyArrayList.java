4039e33829e055fff99eab3091aa78e7fdbc92cd
Gahoo <rmit25@mail.ru>
2023-04-18 23:28:13 +0600

    Add function

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,6 +1,21 @@
 public class MyArrayList<T> implements MyList{
     private T[] arr;
     private int size;
+    MyArrayList() {
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+
+    @Override
+    public void add(Object item) {
+        if (size == arr.length) {
+            T[] newArr = (T[]) new Object[arr.length * 2];
+            for (int i = 0; i < arr.length; i++) {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        }
+    }
     @Override
     public int size() {
         return 0;
@@ -11,10 +26,6 @@ public class MyArrayList<T> implements MyList{
         return false;
     }
 
-    @Override
-    public void add(Object item) {
-
-    }
 
     @Override
     public void add(Object item, int index) {
