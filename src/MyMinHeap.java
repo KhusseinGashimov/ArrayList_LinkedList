eeee487c416b48786d6b8c13595484834359eef9
Gahoo <rmit25@mail.ru>
2023-04-28 15:50:37 +0600

    Declare and Initialize

--- src/MyMinHeap.java
+++ src/MyMinHeap.java
@@ -1,3 +1,9 @@
 import java.util.ArrayList;
 public class MyMinHeap<E extends Comparable<E>> {
+    private ArrayList<E> heap;
+
+    public MyMinHeap() {
+        heap = new ArrayList<>();
+    }
+
 }
