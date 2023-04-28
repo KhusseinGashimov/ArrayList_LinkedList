3746ce413e74819f8f74a7710d5616bcf77f667e
Gahoo <rmit25@mail.ru>
2023-04-28 08:35:33 +0600

    Main | ArrayListStack check methods

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,23 @@ import  java.util.ArrayList;
 
 public class Main{
     public static void main(String[] args) {
+        MyArrayListStack<String> arrayStack = new MyArrayListStack<>();
+        arrayStack.push("one");
+        arrayStack.push("two");
+        arrayStack.push("three");
 
+
+
+        // ArrayListStack Check how it works
+        //===========================================================
+        // Peek at the top element
+        System.out.println(arrayStack.peek()); // prints three
+        // Pop elements from the stacks
+        System.out.println(arrayStack.pop()); // prints three
+        // Check if the stacks are empty
+        System.out.println(arrayStack.isEmpty()); // prints false
+        // Get the size of the stacks
+        System.out.println(arrayStack.size()); // prints 2
+        // ===========================================================
     }
 }
