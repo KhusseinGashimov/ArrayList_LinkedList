1c5bef70e4e81d2e4384e8759dad6f392de2c4d8
Gahoo <rmit25@mail.ru>
2023-04-28 15:12:43 +0600

    Declare an instance variable of type MyLinkedList inside the class. Create a constructor for the class and initialize the instance variable.

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,3 +1,8 @@
 import java.util.LinkedList;
 public class MyLinkedListQueue<E> {
+    private LinkedList<E> queue;
+
+    public MyLinkedListQueue() {
+        queue = new LinkedList<>();
+    }
 }
