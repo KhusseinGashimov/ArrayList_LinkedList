f4aca675fccfc483b8d76ed44e024a753983be28
Gahoo <rmit25@mail.ru>
2023-04-28 13:57:53 +0600

    LinkedListStack checking the methods in a main file

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,35 @@ import  java.util.ArrayList;
 
 public class Main{
     public static void main(String[] args) {
+
+        // Create ArrayListStack&LinkedListStack examples
         MyArrayListStack<String> arrayStack = new MyArrayListStack<>();
-        arrayStack.push("one");
-        arrayStack.push("two");
-        arrayStack.push("three");
+        MyLinkedListStack<Integer> linkedStack = new MyLinkedListStack<>();
+
+
+        // LinkedListStack Check how it works
+        //============================================================
+        // Push elements onto the stacks
+        linkedStack.push(1);
+        linkedStack.push(2);
+        linkedStack.push(3);
+        // Peek at the top element
+        System.out.println(linkedStack.peek()); // prints 3
+        // Pop elements from the stacks
+        System.out.println(linkedStack.pop()); // prints 3
+        // Check if the stacks are empty
+        System.out.println(linkedStack.isEmpty()); // prints false
+        // Get the size of the stacks
+        System.out.println(linkedStack.size()); // prints 2
+        //===========================================================
 
 
 
         // ArrayListStack Check how it works
         //===========================================================
+        arrayStack.push("one");
+        arrayStack.push("two");
+        arrayStack.push("three");
         // Peek at the top element
         System.out.println(arrayStack.peek()); // prints three
         // Pop elements from the stacks
